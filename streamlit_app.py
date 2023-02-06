f930b936cef4c7539e83a80445b73bcba2a2739d
Rentfire <61361884+rentfire@users.noreply.github.com>
2023-02-06 13:48:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ st.info('Developer Advocate, Content Creator and ex-Professor with an interest i
 
 icon_size = 20
 
-st_button('youtube', 'https://youtube.com/dataprofessor', 'Data Professor YouTube channel', icon_size)
+st_button('Website', 'https://www.ballettstudio-ost.de', 'Ballettstudio Ost', icon_size)
 st_button('youtube', 'https://youtube.com/codingprofessor', 'Coding Professor YouTube channel', icon_size)
 st_button('medium', 'https://data-professor.medium.com/', 'Read my Blogs', icon_size)
 st_button('twitter', 'https://twitter.com/thedataprof/', 'Follow me on Twitter', icon_size)
